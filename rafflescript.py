a7cf6e57563bdec9550f4d5a50dbc0c9c0106988
meenapushpa <67387335+meenapushpa@users.noreply.github.com>
2021-02-03 23:40:05 +0000

    Update rafflescript.py

--- rafflescript.py
+++ rafflescript.py
@@ -5,7 +5,6 @@ import bs4 as bs
 import datetime
 import calendar
 import itertools
-import traceback
 import cfscrape
 from lxml.html import fromstring
 from requests.auth import HTTPProxyAuth
@@ -21,11 +20,26 @@ class Raffle(object):
         self.r = requests.session()
         self.i = 10
         self.headers = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.54'
+            ':authority': 'www.nakedcph.com',
+            ':method': 'GET',
+            ':path': '/xx/904/nike-dunk-hi-retro-prm-fcfs-raffle',
+            ':scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-GB,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
             }
         self.json_headers = {
             'Content-Type': 'application/json',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.54'
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
             }
 
     def freeproxylist(self):
@@ -59,6 +73,7 @@ class Raffle(object):
             formatted_proxy = {
                 'http': f'https://{ip}:{port}/',
             }
+            auth=None
         elif len(proxy_parts) == 4:
             ip, port, user, password = proxy_parts
             formatted_proxy = {
@@ -68,43 +83,57 @@ class Raffle(object):
         formatted_proxy = formatted_proxy['http']
         return formatted_proxy, auth
 
-    def raffle_entry(self,proxy,auth=None):
-        log("Entering raffle")
-        raffle_token_request = 'https://www.nakedcph.com/en/898/nike-x-ambush-dunk-hi-cu7544-600-fcfs-raffle'
-        """tokens, user_agent = cfscrape.get_tokens("https://www.nakedcph.com/en/898/nike-x-ambush-dunk-hi-cu7544-600-fcfs-raffle", proxies={"http": proxy, "https": proxy}, auth=auth, verify=False)"""
-        raffle_token = self.r.get(raffle_token_request, headers = self.headers, proxies={"http": proxy, "https": proxy}, auth=auth, verify=False, timeout=10)
-        raffle_token = str(raffle_token.status_code)
-        print(raffle_token)
-        """
-        payload = {
-            'form[language]': 'en',
-            'form[textfield:FhF0pPr4gdHO]': fname,
-            'form[textfield:HjWDPHuvQXDW]': lname,
-            'form[email:xS8rv0ZpuFDg]': email,
-            'form[dropdown:PzvBvJMvRXrd]': country,
-            'form[landed_at]': '{}'.format(calendar.timegm(time.gmtime())),
-            'form[token]': raffle_token,
-        }
-        raffle_url = 'URL'
-        submit = r.post(raffle_url, headers = headers, data=payload, proxies={"http": proxy, "https": proxy})
-        print(submit.text)
-        """
+    def get_csrf(self,url,proxy,auth=None):
+        page = self.r.get(url, headers = self.headers, proxies={"http": proxy, "https": proxy}, auth=auth, verify=False, timeout=10)
+        log(page.text)
+        soup = bs.BeautifulSoup(page.text, 'lxml')
+        log('Scraping csrf token')
+        csrf = soup.find('input', {'name': '_AntiCsrfToken'}).get('value')
+        log(csrf)
+        return csrf
 
+    def raffle_entry(self,url,proxy,auth=None):
+        try:
+            log('Entering raffle')
+            anticsrftoken = self.get_csrf(url,proxy,auth)
+            raffle_token = r.get(url,headers = self.headers, proxies={"http": proxy, "https": proxy}, auth=auth, verify=False, timeout=10)
+            raffle_token = str(raffle_token.text)
+            payload = {
+                'form[language]': 'en',
+                'form[textfield:FhF0pPr4gdHO]': fname,
+                'form[textfield:HjWDPHuvQXDW]': lname,
+                'form[email:xS8rv0ZpuFDg]': email,
+                'form[dropdown:PzvBvJMvRXrd]': country,
+                'form[landed_at]': '{}'.format(calendar.timegm(time.gmtime())),
+                'form[token]': raffle_token,
+            }
+            submit = r.post(url, headers = self.headers, proxies={"http": proxy, "https": proxy}, auth=auth, verify=False, timeout=10)
+            log(submit.text)
+            log("Successfuly completed the transaction!")
+        except requests.exceptions.RequestException as err:
+            print(err)
+            pass
 
 if __name__ == '__main__':
     ra = Raffle()
+    url = 'https://www.nakedcph.com:443/xx/904/nike-dunk-hi-retro-prm-fcfs-raffle'
     accounts = [
     {"fname":"pete","lname":"james","mail":"petejames@gmail.com","phone":"+33612334455","birthdate":"01/01/1998","shoesize":"42",},
     ]
-    for rafflesubmit in range(ra.i):
-        proxy = next(ra.proxy_swm)
-        proxyparse, httpauth = ra.proxy_parse(proxy)
+    option=input("Enter the proxy type [Manual/Online]: ")
+    if option == "Manual":
+        for rafflesubmit in range(ra.i):
+            proxy = next(ra.proxy_swm)
+            proxyparse, httpauth = ra.proxy_parse(proxy)
+            log('Using the PROXY [ ' + proxyparse + '] for the raffle entries')
+            ra.raffle_entry(url,proxyparse,httpauth)
+            ra.r.cookies.clear()
+    elif option == "Online":
         freeproxy = ra.freeproxylist()
         for freelist in freeproxy:
-            try:
-                log('Using ' + freelist + ' for the registration and raffle entries')
-                ra.raffle_entry(freelist)
-                ra.r.cookies.clear()
-                time.sleep(1)
-            except:
-                log("No luck, Trying next...")
+            freeproxyparse, freehttpauth = ra.proxy_parse(freelist)
+            log('Using the PROXY [ ' + freeproxyparse + '] for the raffle entries')
+            ra.raffle_entry(url,freeproxyparse)
+            ra.r.cookies.clear()
+    else:
+        log("Please choose either Manual or Online proxy")
