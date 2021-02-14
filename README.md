2f5920e624cd2e6ece1f42f4b7b6e88090cae09f
Meena <meenanarayanan18@gmail.com>
2021-02-14 00:06:16 +0000

    adding a new version

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # pythonrafflescrape
 
-RaffleScrape repository is used to track the website request and create a script to test the site.
+Raffle Scrape repo is used to scrape or crawl a website protected with Cloudflare. This is fully written in python requests module with the help of BeautifulSoup and Clourscraper modules to process the requests. Expanding this repo with 2captcha key module to solve the captchas before we fully process the request.
 
-By using requests module, automate the raffle via python script.
+# Installation & Dependencies
+  - Python 3.x
+  - Create a python virtual environment
+  - Install dependencies from requirements.txt
+  - Import the cloudflare root and intermediate SSL certificates into keystore to trust the connection
+
+    `pip install -r requirements.txt` will install the Python dependencies automatically.
+
+# Usage
+
+Execute the command   `python rafflescript.py` and will ask for couple of inputs to proceed further, please answer them and click enter. There are few values are hardcoded as of 13th Feb,2021 and will be variablised once all testing is completed. Few things to note !
+
+- The script will end if it finds any exception during the execution.
+- If you encounter the following error, you may need to re run the script one or more time to see the actual results
+  ```
+  cloudscraper.exceptions.CloudflareChallengeError: Detected a Cloudflare version 2 challenge, This feature is not available in the opensource (free) version.
+
+  ```
+  This doesn't mean that will have to go for newer version as this is compromised for the current requirement using our script. It just needs multiple runs to come output
+
+# Flow of execution
+
+  1. Fetch the input from User
+  2. Proxying the request
+  3. Parsing the request
+  4. Scraping require tokens
+  5. Entering Raffle
+  6. Solving Captcha
+  7. Completing the Transaction.
 
 
------Under Construction----
+# Inflight works
+
+Step 6 and 7 is yet to be completed.
--- rafflescript.py
+++ rafflescript.py
@@ -12,7 +12,7 @@ import cloudscraper
 import sys
 import os
 from twocaptcha import TwoCaptcha
-
+import codecs
 
 
 def log(event):
@@ -23,7 +23,11 @@ class Raffle(object):
     file_proxies = "/root/proxies.txt"
     def __init__(self):
         self.r = requests.session()
-        self.scraper = cloudscraper.create_scraper()
+        self.debug=input("Do you want to see verbose logs ? [y/n] : ")
+        if self.debug == 'y':
+            self.scraper = cloudscraper.create_scraper(debug=True)
+        else:
+            self.scraper = cloudscraper.create_scraper()
         self.i = 5
         self.headers = {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -35,7 +39,15 @@ class Raffle(object):
             'Content-Type': 'application/json',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
             }
-
+        self.subscriber_headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+            'host': 'app.rule.io',
+            'referer': 'https://www.nakedcph.com/'
+        }
+    # function to call public available proxy from free-proxy-list and test the availability against google.com
     def freeproxylist(self):
         freeurl = 'https://free-proxy-list.net/anonymous-proxy.html'
         freeresponse = requests.get(freeurl)
@@ -51,15 +63,15 @@ class Raffle(object):
             except:
                 pass
         return freeproxies
-
+    # function to read proxy list from the user input which takes from the line number 24
     def read_proxies(file_path):
         with open(file_path) as txt_file:
             proxies = txt_file.read().splitlines()
         return proxies
-
+    # Iterating the proxy using itertools and next function to test one by one if we have many in input list / file
     proxies = read_proxies(file_proxies)
     proxy_swm = itertools.cycle(proxies)
-
+    # function to parse the proxy value for requests module to process the https requests in offline
     def proxy_parse(self,proxy):
         proxy_parts = proxy.split(':')
         if len(proxy_parts) == 2:
@@ -76,7 +88,7 @@ class Raffle(object):
             auth=None
         formatted_proxy = formatted_proxy['http']
         return formatted_proxy, auth
-
+    # Extracting the required secrets from the targetted site using BeautifulSoup and cloudscraper module
     def get_sitekey(self,url,proxy,auth=None):
         page = self.scraper.get(url, proxies={"http": proxy, "https": proxy})
         soup = bs.BeautifulSoup(page.text, 'lxml')
@@ -87,7 +99,7 @@ class Raffle(object):
         signuplist = soup.find('button' ,attrs={"class":"g-recaptcha"})
         sitekey=signuplist['data-sitekey']
         return sitekey, sitetoken
-
+    # Initial template of two captcha but not completed fully yet **** IN PROGRESS****
     def twocaptcha(self,sitekey,url):
         sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
         api_key = os.getenv('APIKEY_2CAPTCHA', 'YOUR_API_KEY')
@@ -100,13 +112,16 @@ class Raffle(object):
         except Exception as e:
             sys.exit(e)
         else:
-            print('Solved: ' + str(result))
-
-    def raffle_entry(self,url,proxy,auth=None):
+            return str(result)
+    # function is to process raffle entries by using all the outputs from previous functions
+    def raffle_entry(self,url,suburl,proxy,auth=None):
         try:
             log('Entering raffle')
             tk, siky = self.get_sitekey(url,proxy)
-            payload = {
+            #gresp=self.twocaptcha(siky,url) #This can be enabled only after we retreived the g_captcha_reponse.
+            payload = { # hardcoded for testing and wil be variablised once we fully done
+                'action':'{}'.format(suburl),
+                'id':'rule-optin-form',
                 'tags[]': '75612',
                 'token': '{}'.format(tk),
                 'rule_email': 'petejames@gmail.com',
@@ -118,31 +133,32 @@ class Raffle(object):
                 'fields[Raffle.Postal Code]': '97300',
                 'fields[Raffle.City]': 'Cayenne',
                 'fields[Raffle.Country]': 'FR',
+                'fields[Raffle.Signup Newsletter]': 'on',
                 'fields[SignupSource.ip]': '192.0.0.1',
-                'fields[SignupSource.useragent]': 'Mozilla'
+                'fields[SignupSource.useragent]': 'Mozilla',
+                'language': 'sv',
+                'g-recaptcha-response': '' #This can be set only after we retreived the g_captcha_reponse.
             }
-            submit = self.scraper.post(url,headers=self.headers,data=payload,proxies={"http": proxy, "https": proxy})
-            log(submit.status_code)
-            with open("output.txt", 'w') as outfile:
+            log("Sending payload to the requested URL...")
+            submit = self.scraper.post(url,headers=self.subscriber_headers,data=json.dumps(payload),proxies={"http": proxy, "https": proxy},verify="/root/cacerts")
+            print(submit.status_code)
+            with codecs.open("output.txt", 'w',encoding='utf-8') as outfile: # writing a new file with the ouput for future reference,
                 outfile.write(submit.text)
-            log("Successfuly completed the transaction!")
         except requests.exceptions.RequestException as err:
-            print(err)
-            pass
+              print(err)
 
 if __name__ == '__main__':
     ra = Raffle()
-    url = 'https://www.nakedcph.com:443/xx/904/nike-dunk-hi-retro-prm-fcfs-raffle'
-    accounts = [
-    {"fname":"pete","lname":"james","mail":"petejames@gmail.com","phone":"+33612334455","birthdate":"01/01/1998","shoesize":"42",},
-    ]
+    url = 'https://www.nakedcph.com:443/xx/904/nike-dunk-hi-retro-prm-fcfs-raffle' # hardcoded for testing and wil be variablised once we fully done
+    suburl = 'https://app.rule.io/subscriber-form/subscriber' # hardcoded for testing and wil be variablised once we fully done
     option=input("Enter the proxy type [Manual/Online]: ")
+    # Processing the modules based on user request
     if option == "Manual":
         for rafflesubmit in range(ra.i):
             proxy = next(ra.proxy_swm)
             proxyparse, httpauth = ra.proxy_parse(proxy)
             log('Using the PROXY [ ' + proxyparse + '] for the raffle entries')
-            ra.raffle_entry(url,proxyparse,httpauth)
+            ra.raffle_entry(url,suburl,proxyparse,httpauth)
             ra.r.cookies.clear()
     elif option == "Online":
         freeproxy = ra.freeproxylist()
@@ -150,8 +166,7 @@ if __name__ == '__main__':
         for freelist in freeproxy:
             freeproxyparse, freehttpauth = ra.proxy_parse(freelist)
             log('Using the PROXY [ ' + freeproxyparse + '] for the raffle entries')
-            ra.raffle_entry(url,freeproxyparse)
+            ra.raffle_entry(url,suburl,freeproxyparse)
             ra.r.cookies.clear()
     else:
         log("Please choose either Manual or Online proxy")
-
