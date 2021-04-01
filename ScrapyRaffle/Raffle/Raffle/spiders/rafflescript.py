ae5154034dc214e576bbe2c830575439871cbff5
meenapushpa <67387335+meenapushpa@users.noreply.github.com>
2021-04-01 13:52:13 +0100

    Update and rename formrequest/Formrequest_raffle.py to ScrapyRaffle/Raffle/Raffle/spiders/rafflescript.py

--- ScrapyRaffle/Raffle/Raffle/spiders/rafflescript.py
+++ ScrapyRaffle/Raffle/Raffle/spiders/rafflescript.py
@@ -91,8 +91,7 @@ class RafflescriptSpider(scrapy.Spider):
 
 	def parse(self,response):
 		#tk= self.get_sitekey()
-		data={'tags[]': '76713', 'token': 'c812c1ff-2a5a0fe-efad139-d754416-71e1e60-2ce', 'rule_email': 'wlslakshman@gmail.com', 'fields[Raffle.Instagram Handle]': 'Lakshmanan', 'fields[Raffle.Phone Number]': '+44 78 3063 3285', 'fields[Raffle.First Name]': 'Lakshmanan', 'fields[Raffle.Last Name]': 'S', 'fields[Raffle.Shipping Address]': "Queens Road", 'fields[Raffle.Postal Code]': "NE61 2TD", 'fields[Raffle.City]': "WEST EDINGTON", 'fields[Raffle.Signup Newsletter]': '1', 'fields[SignupSource.ip]': '192.0.0.1', 'fields[SignupSource.useragent]': 'Mozilla', 'email_field': '1', 'language': 'sv', 'g-recaptcha-response': '03AGdBq26csmo47MS8grr0txAzzqpIXHAw3euzZzQ4HDYOInPXZK5S9dOqeWvdmVLKLQXQV4A85v4fydOvXByN1_SQCQmo4EInwriPF1uwhNXBxC6deErcUy50pw_vGnllfYoqO3EbyjLZGaXGg1WB9vQgDYC_Ir_KyiUnCCnQE4QZyktAtk_oZwD4oJDeUbRxs40XSwVyqUl24OwTO1HXQCugT3VKudLNbOLHgyy0ZVd5JedsAKHBb0J-wTN7c3puW0sOKA8jsZ2CdUx_dPhH6NlbfrAn2orypPJvskYCQVZpBSoIZ6Gjgy0BWu7wcs43Z0Dl6HdDG3EN5cmZ-rLNgofkpxL6BoqBKJd37MS93Ny_nmIgnJMs-7r8pcZFmr32YsdRKWXzQjARKK8obWWWmkA4d62AEyLZdj7qmG_Q4cg_oN5Bepy8hntmWGXYXe-vVGIENdlLhEfYIwiq6TQlfjL8r248F2R_ZxJ_AEkUcyAz1_0ExEBajqP3_yatRcRHoErUTj2j8Qd-fX1qWmOvftFx6D761FgCNg'}
-		"""first_form = self.get_all_forms()
+		first_form = self.get_all_forms()
 		form_details = self.get_form_details(first_form)
 		buttonvalue = form_details["button"]
 		log("Resolving Captcha")
@@ -106,5 +105,5 @@ class RafflescriptSpider(scrapy.Spider):
 			for button_tag in buttonvalue:
 				if button_tag["class"] == "g-recaptcha":
 					data["g-recaptcha-response"]=gresp
-			data['fields[Raffle.Country]']="GB" """
-		yield FormRequest.from_response(response,formnumber=1,formdata=data,clickdata={'name': 'Sign up!'})
+			data['fields[Raffle.Country]']="GB"
+		yield scrapy.Request('https://www.nakedcph.com/xx/904/nike-dunk-hi-retro-prm-fcfs-raffle',data=data)
